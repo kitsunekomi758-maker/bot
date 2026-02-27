c5e3dc9a5d99e2be00a3f58640fa2dd7d5be7141
kitsunekomi758-maker <kitsunekomi758@gmail.com>
2026-02-26 22:07:10 -0700

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,12 @@
+from flask import flask
+from threading import Thread
+
+app = flask('')
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
+def run():
+    app.run(host='0.0.0.0', port=8080)
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
